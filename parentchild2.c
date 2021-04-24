e2c16f147094b7952ba2f307926d03afce78b574
MicroboyT-T <muazsolihin11@gmail.com>
2021-04-24 15:00:33 +0000

    second commit

---
+++ parentchild2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+	int pid;	/* process ID */
+
+	switch (pid = fork()) {
+	case 0:		/* a fork returns 0 to the child */
+		printf("I am the child process: pid=%d\n", getpid());
+		break;
+
+	default:	/* a fork returns a pid to the parent */
+		wait(NULL);
+		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
+		break;
+
+	case -1:	/* something went wrong */
+		perror("fork");
+		exit(1);
+	}
+	exit(0);
+}
